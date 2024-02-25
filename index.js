73c540ea2e407cb0b88db5e677e212a2857c5cff
sanhuo2014 <zl.gz@qq.com>
2024-02-25 15:52:03 +0800

    0.0.2 commit

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const app = express();
 const accountLoginApp = require('account-login-app');
+const btcToday = require('founction-btc-today');
 
-// 设置 ejs 为视图引擎
+app.use(express.json());
+
 app.set('view engine', 'ejs');
 
-// 登录逻辑
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   const isAuthenticated = accountLoginApp.login(username, password);
 
   if (isAuthenticated) {
-    // 如果登录成功，渲染 home 视图
     res.render('home');
   } else {
-    // 如果登录失败，返回错误信息
     res.status(401).send('Login failed');
   }
 });
 
-// 监听 3000 端口
+app.get('/getprice', async (req, res) => {
+  try {
+    let price = await btcToday();
+    res.json({price: price});
+  } catch (error) {
+    res.json({error:'获取价格失败.'});
+  }
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
